b308aad945a27072b70675cb53c679ab0019347e
Oswaldo-Gabriel-Villaverde-Mendoza <149032380+Oswaldo-Gabriel-Villaverde-Mendoza@users.noreply.github.com>
2024-12-11 00:08:47 -0600

    Stacks Test 3 _ HanoiGame

--- learning/Stacks/stack_sandbox.py
+++ learning/Stacks/stack_sandbox.py
@@ -1,19 +1,38 @@
 from Node import Node
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 class Stack:
-    def __init__(self):
+    def __init__(self, limit=1000):
         self.top_item = None
+        self.size = 0
+        self.limit = limit
+    def has_space(self):
+        return self.size < self.limit
+    def is_empty(self):
+        return self.size == 0
     def peek(self):
-        if self.top_item is None:
-            raise AttributeError("Stack is empty. No top item to peek.")
-        return self.top_item.get_value()
+        if not self.is_empty():
+            return self.top_item.get_value()
+        else:
+            logger.warning("La pila esta totalmente vacia!")
+            return None
     def push(self, value):
-        item = Node(value)
-        item.set_next_node(self.top_item)
-        self.top_item = item
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+        else:
+            logger.warning("La pila esta llena ¡No queda espacio!")
     def pop(self):
-        if self.top_item is None:
-            raise AttributeError("Stack is empty. No item to pop.")  
-        item_to_remove = self.top_item
-        self.top_item = item_to_remove.get_next_node()
-        return item_to_remove.get_value()
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            logger.warning("La pila esta totalmente vacia!")
+            return None
